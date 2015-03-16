baffd0510f52160372fb9e426e244142c5e0c109
Omer Wazir <omer.wazir@gmail.com>
2015-03-15 23:53:05 -0700

    Stub methods for now

---
+++ exploring_arduino/rgb_nightlight/rgb_nightlight.ino
@@ -0,0 +1,24 @@
+const int BLED = 9;
+const int GLED = 10;
+const int RLED = 11;
+const int BUTTON = 2;
+
+boolean lastButton = LOW;
+boolean currentButton = LOW:
+int ledMode = 0;
+
+void setup(){
+//TODO
+}
+
+boolean debounce(boolean last){
+//TODO
+}
+
+void setMode(int mode){
+//TODO
+}
+
+void loop(){
+//TODO
+}
